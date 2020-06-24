8c3941760547795b5b1e913fde354b2f35497fec
madukubah <muhalfalah1998@gmail.com>
2020-06-24 12:58:56 +0800

    first

--- __manifest__.py
+++ __manifest__.py
@@ -1,30 +1,24 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': 'POS Cash Back',
+    'name': 'Quality Assurance & Quality Check',
     'version': '1.0',
     'author': 'Technoindo.com',
-    'category': 'Pont of Sales Management',
+    'category': 'Sales Management',
     'depends': [
-        'account',
-        'product',
-        'sale',
-        'point_of_sale',
+        'sale_contract',
     ],
     'data': [
         'views/menu.xml',
-        'views/cashback.xml',
-        'views/pos_cashback_templates.xml',
-        'views/pos_config.xml',
-        'views/pos_order.xml',
-        'views/pos_session.xml',
-        'views/pos_cashback_statement.xml',
+        'views/qaqc_coa.xml',
     ],
     'qweb': [
-        'static/src/xml/cashback_templates.xml',
+        # 'static/src/xml/cashback_templates.xml',
     ],
     'demo': [
         # 'demo/sale_agent_demo.xml',
     ],
-    'installable': True,
+    "installable": True,
+	"auto_instal": False,
+	"application": True,
 }
--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from . import pos_cashback
-from . import pos_config
-from . import pos_order
-from . import pos_session
-from . import pos_cashback_statement
-from . import account_move
+from . import qaqc_coa
+# from . import sale_park_industry
+# from . import partner
+# from . import pos_session
+# from . import pos_cashback_statement
+# from . import account_move
